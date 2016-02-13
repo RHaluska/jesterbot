cf7d01bed901cdf2327080c4e1b5129fcc378197
Rebecca Haluska <rmh111@case.edu>
2016-02-13 16:12:40 -0500

    Fixed the issue with quotes
    Still no word on a less terrible joke API.

--- bot.js
+++ bot.js
@@ -5,19 +5,19 @@ var controller = Botkit.slackbot({
   debug: false,
 });
 
-var version = "0.0.3"
+var version = "0.0.4"
 var townName = "HackCWRU"
 
-
+//Placeholder until I find a decent, legal alternative
 var songs = ["You ever hear Eye of the Tiger?","I highly recommend Eye of the Tiger.", "Top pick? Eye of the Tiger.",
 "I know one that'll blow your mind: Eye of the Tiger.", "The only song I know is Eye of the Tiger.",
 "Eye of the Tiger!","What about Eye of the Tiger?","You know what song always pumps me up? Eye of the Tiger.",
 "Honestly? Eye of the Tiger.","Experts recommend Eye of the Tiger.","How about Eye of the Tiger?",
 "I don't know about you,but I'd love to listen to Eye of the Tiger.","Hmm...Eye of the Tiger.","It's the eye of the tiger, it's the thrill of the fight!"]
 
+//magic
 var request = require('request');
 var url = 'http://api.icndb.com/jokes/random/';
-
 var bot = controller.spawn(
   {
     token:process.env.token
@@ -25,7 +25,9 @@ var bot = controller.spawn(
 ).startRTM();
 
 
-//message_received is NOT a catch-all! The docs lied to us both.
+//General structure of bot's logic. Super Simple Stuff, it's just an assortment of things it listens for and things it
+//does when it hears them.
+//FYI: message_received is NOT a catch-all! The docs lied to us both. Use all the others?
 controller.hears(['^.request_feature (.*)$'],'ambient,direct_mention,mention,direct_message',function(bot,message) {
   var matches = message.text.match(/request_feature (.*)/i);
   feature = matches[1];
@@ -39,11 +41,13 @@ controller.hears(['(^.ls$)','help'],'ambient,direct_mention,mention,direct_messa
   });
 })
 
+//Most fun we have in here, uses Node.js's request lib to pull JSON from a URL. Feel free to change URL.
 controller.hears(['joke'],'ambient,direct_mention,mention,direct_message',function(bot,message) {
- request('http://api.icndb.com/jokes/random/', function (error, response, body) {
+ request('http://api.icndb.com/jokes/random/', function (error, response, data) {
   if (!error && response.statusCode == 200) {
-    var response = JSON.parse(body);
-    bot.reply(message, response.value.joke);
+    var response = JSON.parse(data);
+    response = response.value.joke.replace("&quot;","'\'");
+    bot.reply(message, response);
   } else {
     bot.reply(message,"Got an error: ", error, ", status code: ", response.statusCode);
   }
@@ -56,6 +60,7 @@ controller.hears(['^.name_group (.*)$'],'ambient,direct_mention,mention,direct_m
   bot.reply(message,"Alright, folks, I'm here to entertain " + townName);
 });
 
+//Super simple chat bits from here on down.
 controller.hears(['^.identify$','what'],'ambient,direct_mention,mention,direct_message',function(bot,message) {
       bot.reply(message, 'I am a testbot! I am here to entertain the people of '+townName+', and I am version '+version+'! Type .ls to see commands I recognize.');
 })
@@ -92,6 +97,8 @@ controller.hears(['shutdown','quit','die'],'ambient,direct_mention,mention,direc
       bot.reply(message, 'I can be killed, but not by those words! The shutdown code\'s been sent to the team admin');
 })
 
+//Conversations are a bit fancier. A phrase triggers a conversation, which listens for a pattern, and has a default
+//message in case no valid pattern's detected. bot.utterances.yes and .no cover a bunch of synonyms for yes and no. 
 controller.hears(['blarghlharglwha'],'ambient,direct_mention,mention,direct_message',function(bot,message) {
 
   bot.startConversation(message,function(err,convo) {
